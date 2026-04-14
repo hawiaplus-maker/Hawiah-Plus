0ac6794b08a9dd2e027972b1ca93fcdd780b536a
Mohamedkhaled15 <mohammedkhaled15621@gmail.com>
2026-04-14 19:04:26 +0200

    all done

--- lib/features/layout/presentation/widget/update_checker.dart
+++ lib/features/layout/presentation/widget/update_checker.dart
@@ -1,5 +1,6 @@
 import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:hawiah_client/core/locale/app_locale_key.dart';
 import 'package:new_version_plus/new_version_plus.dart';
 
@@ -26,36 +27,39 @@ class UpdateChecker {
       context: context,
       barrierDismissible: false,
       builder: (context) {
-        return AlertDialog(
-          title: Text(AppLocaleKey.updateTitle.tr()),
-          content: Text(
-            AppLocaleKey.updateContent.tr(
-              namedArgs: {
-                "store_version": status.storeVersion,
-                "local_version": status.localVersion,
-              },
+        return PopScope(
+          canPop: false,
+          child: AlertDialog(
+            title: Text(AppLocaleKey.updateTitle.tr()),
+            content: Text(
+              AppLocaleKey.updateContent.tr(
+                namedArgs: {
+                  "store_version": status.storeVersion,
+                  "local_version": status.localVersion,
+                },
+              ),
             ),
+            actions: [
+              TextButton(
+                onPressed: () => SystemNavigator.pop(),
+                child: Text(AppLocaleKey.cancel.tr()),
+              ),
+              ElevatedButton(
+                onPressed: () async {
+                  const playStoreLink =
+                      "https://play.google.com/store/apps/details?id=com.hawiah.plus";
+                  const appStoreLink =
+                      "https://apps.apple.com/us/app/%D8%AD%D8%A7%D9%88%D9%8A%D8%A9-%D8%A8%D9%84%D8%B3/id6756757283";
+
+                  final link =
+                      Theme.of(context).platform == TargetPlatform.iOS ? appStoreLink : playStoreLink;
+
+                  await newVersion.launchAppStore(link);
+                },
+                child: Text(AppLocaleKey.updateNow.tr()),
+              ),
+            ],
           ),
-          actions: [
-            TextButton(
-              onPressed: () => Navigator.pop(context),
-              child: Text(AppLocaleKey.updateLater.tr()),
-            ),
-            ElevatedButton(
-              onPressed: () async {
-                const playStoreLink =
-                    "https://play.google.com/store/apps/details?id=com.hawiah.plus";
-                const appStoreLink =
-                    "https://apps.apple.com/us/app/%D8%AD%D8%A7%D9%88%D9%8A%D8%A9-%D8%A8%D9%84%D8%B3/id6756757283";
-
-                final link =
-                    Theme.of(context).platform == TargetPlatform.iOS ? appStoreLink : playStoreLink;
-
-                await newVersion.launchAppStore(link);
-              },
-              child: Text(AppLocaleKey.updateNow.tr()),
-            ),
-          ],
         );
       },
     );
